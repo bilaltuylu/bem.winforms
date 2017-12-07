2101ba6eef86128149fdd909e1292261c2a8be30
BEM <BEM@DESKTOP-S3O5AOR>
2017-12-07 14:14:01 +0300

    asdas

--- SinavApp/frmSinavEkrani.Designer.cs
+++ SinavApp/frmSinavEkrani.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmSinavEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 375);
+            this.Name = "frmSinavEkrani";
             this.Text = "frmSinavEkrani";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
